e67cf5082e32bb414da7772df1f486b545e632fb
Francisco Fernandes <franci.fernand@ua.pt>
2024-03-23 12:53:55 +0000

    quizzy

---
+++ quizzy/lib/data/questions.dart
@@ -0,0 +1,58 @@
+import 'package:quizzy/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    'What is the capital city of Italy?',
+    [
+      'Rome',
+      'Paris',
+      'Madrid',
+      'Berlin',
+    ],
+  ),
+  QuizQuestion(
+    'Who painted the Mona Lisa?', 
+    [
+    'Leonardo da Vinci',
+    'Vincent van Gogh',
+    'Pablo Picasso',
+    'Michelangelo',
+  ]
+  ),
+  QuizQuestion(
+    'What is the traditional Japanese garment called?',
+    [
+      'Kimono',
+      'Sari',
+      'Hanbok',
+      'Dirndl',
+    ],
+  ),
+  QuizQuestion(
+    'In which country is the famous Taj Mahal located?',
+    [
+      'India',
+      'China',
+      'Egypt',
+      'Turkey',
+    ],
+  ),
+  QuizQuestion(
+    'Who wrote the famous play "Romeo and Juliet"?',
+    [
+      'William Shakespeare',
+      'Fyodor Dostoevsky',
+      'Jane Austen',
+      'Charles Dickens',
+    ],
+  ),
+  QuizQuestion(
+    'Which country is known for its flamenco dance and bullfighting?',
+    [
+      'Spain',
+      'Brazil',
+      'Argentina',
+      'Mexico',
+    ],
+  ),
+];
