d07a2baa76b6554131be5f4dc65101be6a34aafc
Francisco Fernandes <franci.fernand@ua.pt>
2024-03-30 02:35:31 +0000

    popups

--- Flutter/quizzy/lib/start_screen.dart
+++ Flutter/quizzy/lib/start_screen.dart
@@ -13,7 +13,7 @@ class StartScreen extends StatelessWidget {
   Widget build(context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 78, 13, 151),
+        backgroundColor: const Color.fromARGB(255, 78, 13, 151),
         elevation: 0,
         leading: IconButton(
           icon: const Icon(Icons.star, color: Colors.white, size: 50),
@@ -86,87 +86,211 @@ class StartScreen extends StatelessWidget {
     );
   }
 
-  void _showOptionsDialog(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Choose an option'),
-          content: SingleChildScrollView(
-            child: ListBody(
-              children: <Widget>[
-                GestureDetector(
-                  child: const Text('Classic Game'),
-                  onTap: () {
-                    Navigator.of(context).pop(); // Close the dialog
-                    startQuiz(
-                        "https://opentdb.com/api.php?amount=30&difficulty=easy&type=multiple"); // Start the quiz for Classic Game
-                  },
-                ),
-                Padding(padding: EdgeInsets.all(8.0)),
-                GestureDetector(
-                  child: Text('Categories'),
-                  onTap: () {
-                    Navigator.of(context).pop(); // Close the first dialog
-                    _showCategoriesDialog(
-                        context); // Call another method to show the categories dialog
-                  },
-                ),
-              ],
-            ),
+void _showOptionsDialog(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return Dialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        elevation: 0.0,
+        backgroundColor: Colors.transparent,
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10.0),
           ),
-        );
-      },
-    );
-  }
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  'Choose an Option',
+                  style: TextStyle(
+                    fontSize: 22.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              _buildOptionItem(
+                context,
+                'Classic Game',
+                "https://opentdb.com/api.php?amount=30&difficulty=easy&type=multiple",
+                backgroundColor: Color.fromARGB(255, 78, 13, 151), // Purple color
+              ),
+              SizedBox(height: 10),
+              _buildOptionItem(
+                context,
+                'Categories',
+                null,
+                onTap: () {
+                  Navigator.of(context).pop(); // Close the dialog
+                  _showCategoriesDialog(context); // Show the categories dialog
+                },
+                backgroundColor: Colors.blueGrey, // Blue grey color
+              ),
+              SizedBox(height: 10),
+            ],
+          ),
+        ),
+      );
+    },
+  );
+}
 
-  void _showCategoriesDialog(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Select a Category'),
-          content: SingleChildScrollView(
-            child: ListBody(
-              children: <Widget>[
-                GestureDetector(
-                  child: Text('Geography'),
-                  onTap: () {
-                    Navigator.of(context).pop();
-                    startQuiz(
-                        "https://opentdb.com/api.php?amount=30&category=22&difficulty=easy&type=multiple");
-                  },
-                ),
-                GestureDetector(
-                  child: Text('History'),
-                  onTap: () {
-                    Navigator.of(context).pop();
-                    startQuiz(
-                        "https://opentdb.com/api.php?amount=30&category=23&difficulty=easy&type=multiple");
-                  },
-                ),
-                GestureDetector(
-                  child: Text('Art'),
-                  onTap: () {
-                    Navigator.of(context).pop();
-                    startQuiz(
-                        "https://opentdb.com/api.php?amount=30&category=25&difficulty=easy&type=multiple");
-                  },
-                ),
-                GestureDetector(
-                  child: Text('Sports'),
-                  onTap: () {
-                    Navigator.of(context).pop();
-                    startQuiz(
-                        "https://opentdb.com/api.php?amount=30&category=21&difficulty=easy&type=multiple");
-                  },
-                ),
-                // Add more categories as needed
-              ],
+Widget _buildOptionItem(
+  BuildContext context,
+  String title,
+  String? url, {
+  VoidCallback? onTap,
+  Color backgroundColor = Colors.transparent,
+}) {
+  return Material(
+    color: Colors.transparent,
+    child: InkWell(
+      onTap: onTap ??
+          () {
+            if (url != null) {
+              Navigator.of(context).pop(); // Close the dialog
+              startQuiz(url); // Start the quiz
+            }
+          },
+      borderRadius: BorderRadius.circular(10.0),
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          color: backgroundColor,
+        ),
+        child: Row(
+          children: <Widget>[
+            Icon(
+              Icons.play_circle_filled,
+              color: Colors.white,
             ),
+            SizedBox(width: 10.0),
+            Text(
+              title,
+              style: TextStyle(
+                fontSize: 16.0,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
+
+void _showCategoriesDialog(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return Dialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        elevation: 0.0,
+        backgroundColor: Colors.transparent,
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10.0),
           ),
-        );
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  'Select a Category',
+                  style: TextStyle(
+                    fontSize: 22.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              _buildCategoryItem(
+                context,
+                'Geography',
+                "https://opentdb.com/api.php?amount=30&category=22&difficulty=easy&type=multiple",
+                Icons.location_on, // Geography icon
+                Color.fromARGB(255, 42, 26, 121), // Blue color for Geography
+              ),
+              SizedBox(height: 10),
+              _buildCategoryItem(
+                context,
+                'History',
+                "https://opentdb.com/api.php?amount=30&category=23&difficulty=easy&type=multiple",
+                Icons.history, // History icon
+                Color.fromARGB(255, 210, 126, 0), // Orange color for History
+              ),
+              SizedBox(height: 10),
+              _buildCategoryItem(
+                context,
+                'Art',
+                "https://opentdb.com/api.php?amount=30&category=25&difficulty=easy&type=multiple",
+                Icons.palette, // Art icon
+                const Color.fromARGB(255, 5, 134, 9), // Green color for Art
+              ),
+              SizedBox(height: 10),
+              _buildCategoryItem(
+                context,
+                'Sports',
+                "https://opentdb.com/api.php?amount=30&category=21&difficulty=easy&type=multiple",
+                Icons.sports_soccer, // Sports icon
+                Color.fromARGB(255, 187, 18, 6), // Red color for Sports
+              ),
+              // Add more categories as needed
+              SizedBox(height: 10),
+            ],
+          ),
+        ),
+      );
+    },
+  );
+}
+
+Widget _buildCategoryItem(BuildContext context, String title, String url, IconData iconData, Color color) {
+  return Material(
+    color: Colors.transparent,
+    child: InkWell(
+      onTap: () {
+        Navigator.of(context).pop();
+        startQuiz(url);
       },
-    );
-  }
+      borderRadius: BorderRadius.circular(10.0),
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          color: color, // Assigning specific color to each category
+        ),
+        child: Row(
+          children: <Widget>[
+            Icon(
+              iconData,
+              color: Colors.white,
+            ),
+            SizedBox(width: 10.0),
+            Text(
+              title,
+              style: TextStyle(
+                fontSize: 16.0,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
 }
