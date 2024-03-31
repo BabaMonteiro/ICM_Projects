806a37ffa1611d1ebeb19b37308ca261bfecbd67
BabaMonteiro <barbarammoreira@ua.pt>
2024-03-31 18:40:32 +0100

    Movement mode done

--- Flutter/quizzy/lib/services/sensors.dart
+++ Flutter/quizzy/lib/services/sensors.dart
@@ -2,36 +2,37 @@ import 'package:sensors_plus/sensors_plus.dart';
 import 'dart:async';
 
 class SensorChallengeService {
-  StreamSubscription? _accelerometerSubscription;
   StreamSubscription? _userAccelerometerSubscription;
   final Function shakeDetected;
   final Function upsideDown;
+  bool isUpsideDownDetected = false;
+  Timer? _debounceTimer;
 
   SensorChallengeService({required this.shakeDetected, required this.upsideDown});
 
   void startListening() {
-    _accelerometerSubscription = userAccelerometerEvents.listen(_onUserAccelerometerEvent);
-    _userAccelerometerSubscription = userAccelerometerEvents.listen(_onUserAccelerometerEvent);
+    _userAccelerometerSubscription = userAccelerometerEventStream().listen(_onUserAccelerometerEvent);
   }
 
   void stopListening() {
-    _accelerometerSubscription?.cancel();
     _userAccelerometerSubscription?.cancel();
-  }
-
-  void _onAccelerometerEvent(AccelerometerEvent event) {
-    // Implement shake detection logic here and call onShakeDetected when a shake is detected
+    _debounceTimer?.cancel();
   }
 
   void _onUserAccelerometerEvent(UserAccelerometerEvent event) {
-    // Detect rapid movements for shake detection
+    // Shake detection logic remains unchanged
     if (event.x.abs() > 12 || event.y.abs() > 12 || event.z.abs() > 12) {
       shakeDetected();
     }
 
-    // Implement upside-down detection logic here and call onUpsideDownDetected when the condition is met
-    if (event.z < -9.8) {
+    // Improved upside-down detection with a simple debounce mechanism
+    if (event.z < -9.8 && !isUpsideDownDetected) {
+      isUpsideDownDetected = true;
       upsideDown();
+      _debounceTimer?.cancel(); // Cancel any existing timer
+      _debounceTimer = Timer(Duration(seconds: 2), () {
+      isUpsideDownDetected = false;
+      });
     }
   }
 }
